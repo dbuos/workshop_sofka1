9cabf3d429730a3cde63efd66f4b504b5d7db474
Daniel Bustamante Ospina <daniel.bustamante@sofka.com.co>
2019-05-10 16:33:07 -0500

    Demo base

---
+++ src/main/java/co/sofka/examples/demoSaludadora/SaludoService.java
@@ -0,0 +1,15 @@
+package co.sofka.examples.demoSaludadora;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class SaludoService {
+
+    @GetMapping(value = "/salud", produces = MediaType.TEXT_PLAIN_VALUE)
+    public Mono<String> salud() {
+        return Mono.just("Daniel");
+    }
+}
